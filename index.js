78173af3d053aae87d5bbf3258c2f8faf7e822e3
paveldigana <pavel.digana@email.cz>
2022-12-07 11:07:08 +0100

    changed package.json

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('inputDir');
-    console.log(`Hello ${nameToGreet}!`);
+    const inputDir = core.getInput('inputDir');
+    console.log(`Hello ${inputDir}!`);
+
+    // read the files in the directory
+    const files = fs.readdirSync(inputDir);
+
+    // iterate over the files
+    for (const file of files) {
+        // print the file name
+        console.log(file);
+    }
+
+
+
     const time = (new Date()).toTimeString();
     core.setOutput("outputDir", "dir/" + time);
     // Get the JSON webhook payload for the event that triggered the workflow
