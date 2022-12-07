e26b3e038bcbcf9c0e7d34b4795b373fe236b748
paveldigana <pavel.digana@email.cz>
2022-12-07 13:24:20 +0100

    list of files in the directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pwd
-ls
-cd /create-markdown-action
-pwd
-ls
 echo 'Dist:>>>'
+pwd
 ls dist
+cd /create-markdown-action
+echo '/create-markdown-action'
+pwd
+ls
 npm run create
 
 # chown -R $(ls -ldn /github/workspace/ | awk '{print $3":"$4}') /github/workspace/dist/
